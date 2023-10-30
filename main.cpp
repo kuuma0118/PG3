6ba66e5877416816808d5dfe0233c1c86cd4152f
k022g0093@g.neec.ac.jp <k022g0093@g.neec.ac.jp>
2023-10-30 11:17:49 +0900

    01_03

--- main.cpp
+++ main.cpp
@@ -1,36 +1,51 @@
 #include <stdio.h>
+#include <Windows.h>
+#include <stdlib.h>
+#include <time.h>
 
-int NormalWage(int workedTime) {
-	return 1072 * workedTime;
+typedef int (*func)();
+
+// 半(奇数)ならtrue、丁(偶数)ならfalse
+int GetRandomAnswer() {
+	return rand() % 6 + 1;
 }
 
-int RecursiveWage(int workedTime, int preHourWage = 100) {
-	if (workedTime <= 0) {
-		return 0;
+// 指定された数秒待ってから関数を実行する関数
+void CallBack(func p, bool ans, int second) {
+	printf("結果は");
+	for (int i = 0; i < second; i++) {
+		printf(".");
+		Sleep(1000);
 	}
-	else if (workedTime <= 1) {
-		return 100;
+	int num = p();
+	printf("\n結果：%d\n", num);
+	// 結果によって分岐
+	if (ans == num % 2) {
+		printf("正解！\n");
 	}
-
-	// 現在の時間の時給を計算
-	int currentHourWage = preHourWage * 2 - 50;
-	return  currentHourWage + RecursiveWage(workedTime - 1, currentHourWage);
+	else {
+		printf("残念！\n");
+	}
 }
 
 int main() {
-	int workedTime = 0;
+	// 乱数初期化
+	srand((unsigned int)time(nullptr));
+
+	// プレイヤーの選択
+	int Choice;
+	// 関数ポインタを宣言
+	func pfunc = &GetRandomAnswer;
 
 	while (true) {
-		printf("労働時間を入力してください（0以下の数字で終了） -> ");
-		scanf_s("%d", &workedTime);
-
-		if (workedTime < 0) {
+		printf("半(奇数)なら1、丁(偶数)なら0を入力してください。（それ以外の数字で終了） -> ");
+		scanf_s("%d", &Choice);
+		// 0か1以外なら終了
+		if (Choice != 0 && Choice != 1) {
 			break;
 		}
 
-		printf("一般的な賃金体系の場合 ... %d 円\n", NormalWage(workedTime));
-		printf("再帰的な賃金体系の場合 ... %d 円\n", RecursiveWage(workedTime));
+		// 結果を確認
+		CallBack(pfunc, (bool)Choice, 3);
 	}
-
-	return 0;
 }
