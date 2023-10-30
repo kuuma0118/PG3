501adb6845325590f74c8afdfecf2bdef5812164
k022g0093@g.neec.ac.jp <k022g0093@g.neec.ac.jp>
2023-10-30 12:39:18 +0900

    01_04

--- main.cpp
+++ main.cpp
@@ -1,51 +1,48 @@
 #include <stdio.h>
-#include <Windows.h>
 #include <stdlib.h>
+#include <functional>
+#include <Windows.h>
 #include <time.h>
 
-typedef int (*func)();
-
-// 半(奇数)ならtrue、丁(偶数)ならfalse
-int GetRandomAnswer() {
-	return rand() % 6 + 1;
-}
-
-// 指定された数秒待ってから関数を実行する関数
-void CallBack(func p, bool ans, int second) {
-	printf("結果は");
-	for (int i = 0; i < second; i++) {
-		printf(".");
-		Sleep(1000);
-	}
-	int num = p();
-	printf("\n結果：%d\n", num);
-	// 結果によって分岐
-	if (ans == num % 2) {
-		printf("正解！\n");
-	}
-	else {
-		printf("残念！\n");
-	}
-}
-
 int main() {
-	// 乱数初期化
 	srand((unsigned int)time(nullptr));
 
-	// プレイヤーの選択
 	int Choice;
-	// 関数ポインタを宣言
-	func pfunc = &GetRandomAnswer;
+	
+	std::function<int()> answer = []() {return rand() % 6 + 1; };
+
+	std::function<void(std::function<int()>, bool, int)> callBack = [](std::function<int()> func, bool ans, int second) {
+
+		int num = func();
+
+		printf("結果は");
+		for (int i = 0; i < second; i++) {
+			printf(".");
+			Sleep(1000);
+		}
+
+		printf("%d\n", num);
+		
+		if (ans == num % 2) {
+			printf("正解！\n");
+		}
+
+		else {
+			printf("残念！\n");
+		}
+
+	};
 
 	while (true) {
+
 		printf("半(奇数)なら1、丁(偶数)なら0を入力してください。（それ以外の数字で終了） -> ");
+
 		scanf_s("%d", &Choice);
-		// 0か1以外なら終了
+		
 		if (Choice != 0 && Choice != 1) {
 			break;
 		}
 
-		// 結果を確認
-		CallBack(pfunc, (bool)Choice, 3);
+		callBack(answer, (bool)Choice, 3);
 	}
 }
