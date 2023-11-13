7b6fdf030dde80753c33f463d6b09b26cd7706a4
k022g0093@g.neec.ac.jp <k022g0093@g.neec.ac.jp>
2023-11-13 10:33:53 +0900

    02_02

--- IAnimal.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <iostream>
-
-class IAnimal {
-public:
-	// 鳴く
-	virtual void MakeSound() = 0;
-};
--- main.cpp
+++ main.cpp
@@ -1,19 +1,22 @@
-#include "Dog.h"
-#include "Cat.h"
-#include "Human.h"
-#include <windows.h>
-
+#include <stdio.h>
+#include "Comparator.h"
 
 int main() {
-	IAnimal* animals[3]{
-		new Dog(),
-		new Cat(),
-		new Human()
-	};
+	// 比較する数値のの組み合わせ(左の値のほうが小さい)
+	Comparator<int, int> comparator1(1, 2);
+	Comparator<int, float> comparator2(1, 1.5f);
+	Comparator<int, double> comparator3(1, 1.6);
+	Comparator<float, float> comparator4(1.5f, 2.5f);
+	Comparator<float, double> comparator5(1.5f, 1.6);
+	Comparator<double, double> comparator6(1.6, 2.6);
 
-	for (int i = 0; i < 3; i++) {
-		animals[i]->MakeSound();
-	}
+	// 数値の小さいほうを表示
+	printf("<int, int>       %d\n", comparator1.Min<int>());
+	printf("<int, float>     %f\n", comparator2.Min<float>());
+	printf("<int, double>    %lf\n\n", comparator3.Min<double>());
+	printf("<float, float>   %f\n", comparator4.Min<float>());
+	printf("<float, double>  %lf\n\n", comparator5.Min<double>());
+	printf("<double, double> %lf\n", comparator6.Min<double>());
 
 	return 0;
 }
