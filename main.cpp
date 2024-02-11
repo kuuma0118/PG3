5efe54055e01d8d6e5ad87c8f4e5cf250408a63f
渡邉空馬 <k022g0093@g.neec.ac.jp>
2024-02-12 06:43:22 +0900

    01_04

--- main.cpp
+++ main.cpp
@@ -4,62 +4,55 @@
 #include <stdlib.h>
 #include <functional>
 
+// 結果発表
+void SetTimeout(std::function<void()> result, int waitTime) {
+	printf("正解は...?\n");
+	Sleep(1000 * waitTime);
+	result();
+};
+
 int main() {
 	// 乱数の初期値を生成
 	srand((unsigned int)time(nullptr));
-	// サイコロの目をランダムで決定
-	std::function<int()> randomDice = [&]() {
-		int result = rand() % 6 + 1;
-		return result;
-	};
 
-	// 指定した秒数処理を止める
-	std::function<void(int)> SetTimeout = [](int waitTime) {
-		Sleep(1000 * waitTime);
-	};
+	// 入力した予想
+	int selectAnswer;
+
+	printf("サイコロは丁( 2 )、半( 1 )どっちだ\n\n");
+	printf("丁( 2 )、半( 1 )どちらかを入力\n");
+	printf("予想:");
+	scanf_s("%d", &selectAnswer);
 
 	// 結果発表用の変数
-	std::function<void(int, int, int)> result = [&](int answerDice, int selectAnswer, int waitTime) {
-		printf("勝敗は...\n");
-		// 3秒間処理を停止
-		SetTimeout(waitTime);
+	std::function<void()> result = [&]() {
+		// ランダムでサイコロの目を決定
+		int diceNum = rand() % 6 + 1;
 
 		// 正解のサイコロ
-		int answerDiceNum = answerDice % 2;
+		int answerDiceNum = diceNum % 2;
 		// 予想したサイコロ
 		int selectDiceNum = selectAnswer % 2;
 
 		// 偶数なら丁
 		if (answerDiceNum == 0) {
 			printf("丁!\n");
-			SetTimeout(waitTime);
+
 		}// 奇数なら半
 		else if (answerDiceNum != 0) {
 			printf("半!\n");
-			SetTimeout(waitTime);
-		}
 
+		}
 		// どちらも同じ数なら正解
 		if (selectDiceNum == answerDiceNum) {
-			printf("勝利\n\n");
+			printf("正解!!!\n\n");
 		}
 		else {
-			printf("敗北\n\n");
+			printf("不正解!!!\n\n");
 		}
 	};
 
-	// 入力した予想
-	int selectAnswer;
-
-	printf("サイコロは丁( 2 )又は半( 1 )どちらか\n\n");
-	while (1) {
-		printf("丁( 2 )、半( 1 )どちらかを入力\n");
-		printf("予想:");
-		scanf_s("%d", &selectAnswer);
-
-		// 結果発表
-		result(randomDice(), selectAnswer, 3);
-	}
+	// 結果発表
+	SetTimeout(result, 3);
 
 	return 0;
 }
